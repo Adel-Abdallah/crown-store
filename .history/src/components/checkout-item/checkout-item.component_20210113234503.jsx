015fbe2de82e60a33fabc5c012448f40ee557054
Adel-Abdallah <adelgafo@gmail.com>
2021-01-14 00:32:47 +0200

    added addItem and removeItem functionality to checkout item component

---
+++ .history/src/components/checkout-item/checkout-item.component_20210113234503.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { clearItemFromCart } from '../../redux/cart/cart.actions'
+import './checkout-item.styles.scss';
+
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+    <div className="checkout-item">
+        <div className="image-container">
+            <img src={imageUrl} alt="item" />
+        </div>
+        <spn className="name">{name}</spn>
+        <spn className="quantity">{quantity}</spn>
+        <span className="price">{price}</span>
+        <div className="remove-button">&#10005;</div>
+    </div>
+);
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+
+export default connect(null,)(CheckoutItem);
